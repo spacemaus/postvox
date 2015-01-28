605881cf3c7e5d930db3ad1de3752dc4bd43573c
spacemaus <spacemaus@spacemaus.com>
2015-01-28 10:28:15 -0800

    Use dev-specific dbDir and configFile when NODE_ENV=development

--- vox-client/argv.js
+++ vox-client/argv.js
@@ -11,28 +11,35 @@ module.exports = exports = commandLineArgs;
 var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.HOMEDIR || process.cwd();
 
 /**
+ * The Hub is like the root DNS server for Postvox nicknames.  Setting --hubUrl
+ * lets you point to a different Hub authority.
+ */
+exports.hubUrl;
+
+/**
+ * The default interchange URL is used during the `init` command.
+ */
+exports.defaultInterchangeUrl;
+
+/**
  * The config file stores the user's identity and private encryption keys.
  */
-exports.configFile = commandLineArgs.configFile || path.join(homeDir, '.voxconfig.json');
+exports.configFile;
 
 /**
  * The database file stores the user's subscriptions, received messages, etc.
  */
-exports.dbDir = commandLineArgs.dbDir || path.join(homeDir, '.voxhistory');
+exports.dbDir;
+
 
 if (process.env.NODE_ENV == 'development') {
-  /**
-   * The Hub is like the root DNS server for Postvox nicknames.  Setting --hubUrl
-   * lets you point to a different Hub authority.
-   */
   exports.hubUrl = urlparse.parse(commandLineArgs.hubUrl || 'http://localhost:9090');
-
-  /**
-   * The default interchange URL is used during the `init` command.
-   */
   exports.defaultInterchangeUrl = commandLineArgs.defaultInterchangeUrl || 'http://localhost:9454'
-
+  exports.dbDir = commandLineArgs.dbDir || path.join(homeDir, '.voxhistory-dev');
+  exports.configFile = commandLineArgs.configFile || path.join(homeDir, '.voxconfig-dev.json');
 } else {
   exports.hubUrl = urlparse.parse(commandLineArgs.hubUrl || 'http://hub.postvox.net');
   exports.defaultInterchangeUrl = commandLineArgs.defaultInterchangeUrl || 'http://vanilla.postvox.net';
+  exports.dbDir = commandLineArgs.dbDir || path.join(homeDir, '.voxhistory');
+  exports.configFile = commandLineArgs.configFile || path.join(homeDir, '.voxconfig.json');
 }
